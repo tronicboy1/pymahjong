51a5b0b631da9a252ba17eb0648ce94201d9a5cb
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-08-01 13:35:07 +0900

    ChatをBootstrapでスタイリング

--- pyjong/apps/chat/chat.py
+++ pyjong/apps/chat/chat.py
@@ -3,25 +3,21 @@ from pyjong import socketio
 from flask_login import current_user
 from flask import session
 
-    #information to join room will be held in session
-    #function to join a room provided a users session data
+#information to join room will be held in session
+#function to join a room provided a users session data
 @socketio.on('joined', namespace='/main/game')
 def joined(message):
-    """Sent by clients when they enter a room.
-    A status message is broadcast to all people in the room."""
     room = session['room']
     print('joined')
     join_room(room)
-    emit('status', {'msg': session.get('username') + ' has entered the room.'}, room=room)
+    emit('status', {'msg': session['username'] + 'がチャットルームに入りました'}, room=room)
 
 
 @socketio.on('text', namespace='/main/game')
 def text(message):
-    """Sent by a client when the user entered a new message.
-    The message is sent to all people in the room."""
     room = session['room']
     print('text')
-    emit('message', {'msg': session.get('username') + ':' + message['msg']}, room=room)
+    emit('message', {'msg': session['username'] + ' : ' + message['msg']}, room=room)
 
 
 @socketio.on('left', namespace='/main/game')
