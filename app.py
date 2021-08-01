817ad2f4a7679323d582d96cbd8f4c676bfc123d
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-08-01 12:39:45 +0900

    socketio関連のFuncをchat.pyに戻した

--- app.py
+++ app.py
@@ -31,36 +31,6 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-@socketio.on('joined', namespace='/main/game')
-def joined(message):
-    """Sent by clients when they enter a room.
-    A status message is broadcast to all people in the room."""
-    room = session['room']
-    print('joined')
-    join_room(room)
-    emit('status', {'msg': session.get('username') + ' has entered the room.'}, room=room)
-
-
-@socketio.on('text', namespace='/main/game')
-def text(message):
-    """Sent by a client when the user entered a new message.
-    The message is sent to all people in the room."""
-    room = session['room']
-    print('text')
-    emit('message', {'msg': session.get('username') + ':' + message['msg']}, room=room)
-
-
-@socketio.on('left', namespace='/main/game')
-def left(message):
-    """Sent by clients when they leave a room.
-    A status message is broadcast to all people in the room."""
-    room = session['room']
-    leave_room(room)
-    print('left room')
-    emit('status', {'msg': session.get('username') + ' has left the room.'}, room=room)
-
-
-
 if __name__ == '__main__':
     socketio.run(app,debug=True)
     #app.run(debug=True)
