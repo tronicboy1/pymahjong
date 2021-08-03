8a045d3f0354eebcbf37387c70bbba53bd6f992a
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-08-03 12:47:12 +0900

    ポンの捨て牌を捨てるコードが抜けていた

--- pyjong/apps/socketioapps/mahjongsocketio.py
+++ pyjong/apps/socketioapps/mahjongsocketio.py
@@ -141,6 +141,7 @@ def gamecontrol(choice):
             choice = int(choice)
             #check to make sure input is in valid range
             if choice < len(room_dict[session['room']][0].kyoku.current_player.can_sutehai):
+                room_dict[session['room']][0].kyoku.current_player.pon_user_input(choice)
                 room_dict[session['room']][0].kyoku.current_player = room_dict[session['room']][0].kyoku.player_dict[room_dict[session['room']][0].kyoku.ponkanchi_start_player]
                 room_dict[session['room']][1] = 'cycle'
                 cycle_to_human()
