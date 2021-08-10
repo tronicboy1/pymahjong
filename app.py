37ee4ec7ec5d50261b4c1947f8cbf6c406598134
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-08-10 14:03:45 +0900

    SocketIOのURLを変更
    HTTPSでSocketIOがつながらない問題がある

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def logout():
 print(os.path.dirname(__file__))
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app,cors_allowed_origins=["https://pyjong.herokuapp.com"])
     #,host='0.0.0.0',port=5000
