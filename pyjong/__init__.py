6e278068a20843026ca6520b47e60a99142d8c09
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-08-10 20:53:10 +0900

    qlに変更する

--- pyjong/__init__.py
+++ pyjong/__init__.py
@@ -29,7 +29,7 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SECRET_KEY'] = 'mykey'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pwgpahkpakluol:5501fe5faa5b3f44b4bf4225aad1eead65a137f9a01cc8863d5966930462e942@ec2-44-196-146-152.compute-1.amazonaws.com:5432/d3peihanv904u3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pwgpahkpakluol:5501fe5faa5b3f44b4bf4225aad1eead65a137f9a01cc8863d5966930462e942@ec2-44-196-146-152.compute-1.amazonaws.com:5432/d3peihanv904u3'
 #keep tracking figures off until necessary
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #create database object
