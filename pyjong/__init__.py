1ba0ce1e20299d7e01f05ac2d9ace8bcd4b72bb2
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-08-10 20:18:28 +0900

    HerokuのPostgreSQLデータベースのURIへ接続することを試みる

--- pyjong/__init__.py
+++ pyjong/__init__.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
+#from sqlalchemy.orm import scoped_session,sessionmaker
+#from sqlalchemy.ext.declarative import declarative_base
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
@@ -26,7 +28,8 @@ app = Flask(__name__)
 #get base dir here for path to create db
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SECRET_KEY'] = 'mykey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tygdtcltbosejz:1a622409a191010e308c49a607343f1ab72d2bc66e7f90bba894f5dc42140cb0@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d5g33isn665nuf'
 #keep tracking figures off until necessary
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #create database object
